1ad12954c1a4b2a9ec32aa5d5437ba7dc8d3ed4d
Ivan Ivanov <ivan@ivanovs.info>
2017-03-12 07:04:45 +1300

    Replaced sb_push with explicit code for further debugging

--- src/testsimd.cpp
+++ src/testsimd.cpp
@@ -1,27 +1,46 @@
 #include <stdio.h>
 #include <intrin.h>
+#include <stdlib.h>
 
 #include "include/stb_stretchy_buffer.h"
 
 struct Model {
   __m128 simd;
   int a;
-  int b;
 };
 
 int main() {
-  printf("vasia\n");
-
   Model *models = NULL;
+  int *models_raw = NULL;
 
   for (int i = 0; i < 100; ++i) {
     Model model;
     model.a = i;
-    model.b = i + 1;
-    sb_push(models, model);
+    // models[i] = model;
+
+    {
+      int cur = models_raw ? models_raw[0] : 0;
+      int min_needed = (models_raw ? models_raw[1] : 0) + 1;
+      if (cur < min_needed) {
+        int dbl_cur = 2 * cur;
+        int m = dbl_cur > min_needed ? dbl_cur : min_needed;
+        int itemsize = sizeof(Model);
+
+        models_raw =
+            (int *)realloc(models_raw ? models_raw : 0, itemsize * m + sizeof(int) * 2);
+
+        if (!models) models_raw[1] = 0;
+        models_raw[0] = m;
+        models = (Model *)(models_raw + 2);
+      }
+
+      models[i] = model;
+      models_raw[1] += 1;
+    }
+    // sb_push(models, model);
   }
 
-  for (int i = 0; i < sb_count(models); ++i) {
+  for (int i = 0; i < 100; ++i) {
     printf("%d,", models[i].a);
   }
 
