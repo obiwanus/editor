52d19814979e1e176aeb5714df26b58231ae15a5
Ivan Ivanov <ivan@ivanovs.info>
2017-07-09 17:27:36 +1200

    Compile the proper file on windows

--- build.bat
+++ build.bat
@@ -25,8 +25,8 @@ pushd build
 @del /Q *.pdb > NUL 2> NUL
 @del /Q *.gmi > NUL 2> NUL
 
-rem set FilesToCompile= ..\src\ED_win32.cpp
-set FilesToCompile= ..\src\testsimd.cpp
+set FilesToCompile= ..\src\ED_win32.cpp
+rem set FilesToCompile= ..\src\testsimd.cpp
 
 set run_compilation= cl -Feeditor.exe -I..\src %OptimizeFlags% %CommonCompilerFlags% %FilesToCompile% /link %CommonLinkerFlags%
 
